f828e738037867dc238138386cfdba68b979a664
yashvant <yashvantyadav855@gmail.com>
2023-10-24 16:05:33 +0530

    checking apis

---
+++ app/src/main/java/com/example/compose_tflite/medicine_feature/data/local/demodata/poetrydb.kt
@@ -0,0 +1,8 @@
+package com.example.compose_tflite.medicine_feature.data.local.demodata
+
+data class poetrydbItem(
+    val author: String,
+    val linecount: String,
+    val lines: List<String>,
+    val title: String
+)
