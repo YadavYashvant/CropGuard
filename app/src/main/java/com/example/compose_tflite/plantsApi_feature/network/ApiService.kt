2b43307945780adbb3d1ccfe133c90b00b99557c
yashvant <yashvantyadav855@gmail.com>
2023-10-27 23:33:04 +0530

    making ApiService

---
+++ app/src/main/java/com/example/compose_tflite/plantsApi_feature/network/ApiService.kt
@@ -0,0 +1,23 @@
+package com.example.compose_tflite.plantsApi_feature.network
+import com.example.compose_tflite.plantsApi_feature.model.Movie
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("movielist.json")
+    suspend fun getMovies() : List<Movie>
+
+    companion object {
+        var apiService: ApiService? = null
+        fun getInstance() : ApiService {
+            if (apiService == null) {
+                apiService = Retrofit.Builder()
+                    .baseUrl("https://howtodoandroid.com/apis/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build().create(ApiService::class.java)
+            }
+            return apiService!!
+        }
+    }
+}
